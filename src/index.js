ea86e2c8d99f40778b98ed5b384de7a3a73cb18a
AbdifatahYasin1 <iskatag123@gmail.com>
2023-01-03 13:10:26 +0300

    Setup for LeaderBoard

---
+++ src/index.js
@@ -0,0 +1,46 @@
+import './style.css';
+
+const scores = [{
+  name:"Abdifah",
+  score:20
+},{
+  name:"Cabdiqani",
+  score:22
+},
+{
+  name:"Sahra",
+  score:33
+},
+{
+  name:"ismaaciil",
+  score:90
+},
+{
+  name:"Hana",
+  score:99
+},
+{
+  name:"Yaasiin",
+  score:33
+},
+
+
+]
+
+const element = document.createElement("li");
+
+const render = ()=>{
+  scores.forEach((item, index) =>{
+      element.innerHTML +=`
+      <li class='litem ${index % 2 !== 0 ? 'item' : 'item1'}' >
+    <label class='lname'> ${item.name} : </label> 
+    <label class='lscore'> ${item.score} </label>
+  </li>
+      
+      `;
+  })
+}
+window.onload = () => {
+  render();
+  document.querySelector('#score').appendChild(element);
+};
